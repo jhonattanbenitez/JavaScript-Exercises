5b17a30ff5f887b0c92cb423feedeed0994e1293
jhonattanbenitez <jhonattan.benitez@hotmail.com>
2018-10-03 01:00:05 -0500

    email changes

---
+++ email/js/app.js
@@ -0,0 +1,42 @@
+const email = document.getElementById("email");
+const asunto = document.getElementById("asunto");
+const mensaje = document.getElementById("mensaje");
+const btnEnviar = document.getElementById("enviar");
+
+eventListeners();
+
+function eventListeners(){
+    document.addEventListener("DOMContentLoaded", inicioApp);
+    email.addEventListener("blur", validarCampo);
+    asunto.addEventListener("blur", validarCampo);
+    mensaje.addEventListener("blur", validarCampo);
+
+}
+
+function inicioApp(){
+    btnEnviar.disabled = true;
+}
+
+function validarCampo(){
+    validarLongitud(this);
+    
+}
+
+function validarLongitud(campo){
+    console.log(campo.value.length);
+    if(campo.value.length > 0 ){
+        campo.style.borderBottomColor = "gren";
+        campo.classList.remove("error");
+    }else{
+        campo.style.borderBottomColor = "red";
+        campo.classList.add('error');
+    }
+    if(campo.value.length > 0 ) {
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+   } else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.add('error');
+   }
+}
+
--- script.js
+++ script.js
@@ -4,3 +4,14 @@ rolldown.forEach((element, i) => {
     const delay = i / 4 +"s";
     element.style.animationDelay = delay;
 });
+
+function btnReloaded(){
+   const botonReloaded = document.getElementById("myList");
+   botonReloaded.classList.remove("rolldown-list");
+    setTimeout(function() {
+     botonReloaded.classList.add("rolldown-list");
+    }, 1);
+}
+
+
+

18b59a1298516747c7cc08eb4d5b0495a3ff2969
jhonattanbenitez <jhonattan.benitez@hotmail.com>
2018-10-06 16:07:48 -0500

    ejercicio terminado con prototypes

--- cotizaSeguro/js/app.js
+++ cotizaSeguro/js/app.js
@@ -39,7 +39,7 @@ Seguro.prototype.cotizarSeguro = function(){
 
 function Interfaz() {}
 
-Interfaz.prototype.mostrarError = (mensaje, tipo)=>{
+Interfaz.prototype.mostrarMensaje = function(mensaje, tipo){
     const div = document.createElement('div');
     if(tipo === 'error'){
         div.classList.add('mensaje', 'error');
@@ -50,9 +50,45 @@ Interfaz.prototype.mostrarError = (mensaje, tipo)=>{
     formulario.insertBefore(div, document.querySelector('.form-group'));
     setTimeout(() =>{
         document.querySelector('.mensaje').remove();
-    }, 3000)
+    }, 3000);
 }
 
+//imprime el resultado de la cotización
+
+Interfaz.prototype.mostrarResultado = function(seguro, total){
+    const resultado = document.getElementById('resultado');
+    let marca;
+    switch(seguro.marca){
+        case '1': 
+                marca= 'Americano';
+                break;
+        case '2':
+                marca= 'Asiatico';
+                break;
+        case '3':
+                marca= 'Europeo';
+                break;
+
+    }
+    const div = document.createElement('div');
+    div.innerHTML = `
+        <p class='header'>Tu resumen: </p>
+        <p>Marca: ${marca} </p>
+        <p>Año:   ${seguro.anio} </p>
+        <p>Tipo:  ${seguro.tipo} </p>
+        <p>total: $ ${total}     </p>
+     
+    `;
+    const spinner = document.querySelector('#cargando img');
+    spinner.style.display = 'block';
+    setTimeout(() =>{
+        resultado.appendChild(div);
+        spinner.style.display = 'none';
+    }, 3000)
+
+    console.log(spinner);
+    
+}
 
 
 const formulario = document.getElementById('cotizar-seguro');
@@ -72,11 +108,22 @@ formulario.addEventListener("submit", function(event){
     const interfaz = new Interfaz();
 
     if(marcaSeleccionada === '' || anioSeleccionado === '' || tipo === ''){
-        interfaz.mostrarError('Faltan datos, revirsar el formulario', 'error');
+        interfaz.mostrarMensaje('Faltan datos, revirsar el formulario', 'error');
     }else {
+        //limpiar resultados anteriores
+
+        const resultados = document.querySelector('#resultado div');
+        if(resultados != null){
+            resultados.remove();
+        }
+
         const seguro = new Seguro(marcaSeleccionada, anioSeleccionado, tipo)
         
         const cantidad = seguro.cotizarSeguro();
+
+        interfaz.mostrarResultado(seguro, cantidad);
+        interfaz.mostrarMensaje('Cotizando...', 'correcto');
+
     }
    
 });
